c6027bd1eb8886eba8d8c987d1d34ae7c7431b40
neerajproo <94364617+neerajproo@users.noreply.github.com>
2022-01-16 15:41:29 +0530

    Update git.js

--- plugins/git.js
+++ plugins/git.js
@@ -9,16 +9,16 @@ Asena.addCommand({pattern: 'git', fromMe: false, desc: "Gives github link of the
 
     var skl = await axios.get("https://images.wallpapersden.com/image/download/pokemon-detective-pikachu-4k_a2lrZmWUmZqaraWkpJRsZ2WtbGdl.jpg", { responseType: 'arraybuffer' })
 
-    await message.sendMessage(Buffer(skl.data), MessageType.image, {mimetype: Mimetype.png, caption: `*Pikachu bot created by Ameer Suhail*
+    await message.sendMessage(Buffer(skl.data), MessageType.image, {mimetype: Mimetype.png, caption: `*Venom bot created by Neeraj*
 *Creator number : wa.me/919946432377?text=Hi%20Ameer%20Suhail.%20*
 
 *Bot setting video : https://youtu.be/_D4ZYuUSXjs*
 
-*Githublink (Setup)  :    https://github.com/ameer-kallumthodi/pikachu*
+*Githublink (Setup)  :    https://github.com/neerajproo/Venom*
 
-*Audio commads :   https://github.com/ameer-kallumthodi/pikachu/tree/master/uploads*
+*Audio commads :   https://github.com/neerajproo/Venom/tree/master/uploads*
 
-*Sticker commads : https://github.com/ameer-kallumthodi/pikachu/tree/master/uploads*
+*Sticker commads : https://github.com/neerajproo/Venom/tree/master/uploads*
 
 *Video For Setting Bot : https://youtu.be/_D4ZYuUSXjs* 
 
