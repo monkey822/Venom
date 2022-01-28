387ef93408f9e6d9d6ed096d82822a9339469445
neerajproo <94364617+neerajproo@users.noreply.github.com>
2022-01-28 11:27:48 +0530

    Neeraj🔥

--- plugins/owner.js
+++ plugins/owner.js
@@ -18,7 +18,7 @@ if (Config.WORKTYPE == 'private') {
         }
 
         if (Config.PLK == 'default') {
-            await message.client.sendMessage(message.jid,'Pikachu Bot created by *Ameer Suhail*' , MessageType.text);
+            await message.client.sendMessage(message.jid,'Venom Bot created by *Neeraj Sir*' , MessageType.text);
         }
         else {
             await message.client.sendMessage(message.jid,Config.PLK + '\n\n---------------------', MessageType.text);
